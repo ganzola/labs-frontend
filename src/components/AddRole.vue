68780bb6fad7972214ff2697974e17b35438140a
Gabriel Andres Anzola Tachak <gabriel.anzola@mercadolibre.com.co>
2022-05-08 08:48:36 -0500

    get auth token error on addrole fixed

--- src/components/AddRole.vue
+++ src/components/AddRole.vue
@@ -31,6 +31,7 @@
 
 <script>
   import axios from 'axios';
+  import {getAuthenticationToken} from '@/dataStorage';
 
   export default {
     name: "AddRole",
@@ -62,7 +63,7 @@
               password: this.password
             }, {
               params: {
-                access_token: localStorage.getItem( "token" )
+                access_token: getAuthenticationToken( ),
               }
             }
           ).then( response => {
@@ -84,7 +85,9 @@
       },
       buildURI( ){
         let associatePath = "/registro/nuevo-rol/";
-        return this.$store.state.backURL + associatePath + this.role;
+        let finalPath = this.$store.state.backURL + associatePath + this.role;
+        console.log(finalPath)
+        return finalPath;
       }
     }
 
